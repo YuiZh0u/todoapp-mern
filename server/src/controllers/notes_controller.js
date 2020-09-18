d801c63e94785b07e5f63aca9df00cba8fcbf860
YuiZh0u <yui.zhou97@gmail.com>
2020-09-18 02:08:08 -0300

    add user controller

--- server/src/controllers/notes_controller.js
+++ server/src/controllers/notes_controller.js
@@ -2,12 +2,12 @@
 const notesCtrl = {};
 
 const noteModel = require('../models/note');
-const { render } = require('../app');
+// const { render } = require('../app');
 
 notesCtrl.getAllNotes = async (req,res) => {
   const notas = await noteModel.find(); //because find() es asincrono
   res.json(notas);
-}
+};
 
 notesCtrl.createNote = async (req, res) => {
   const {title, description, date, autor} = req.body; //lo q entrega el POST
@@ -23,7 +23,7 @@ notesCtrl.createNote = async (req, res) => {
 };
 
 notesCtrl.updateNote = async (req, res) => {
-  const {title, description, autor} = req.body
+  const {title, description, autor} = req.body;
   await noteModel.findOneAndUpdate(req.params.id, {
     title: title,
     description: description,
@@ -31,12 +31,12 @@ notesCtrl.updateNote = async (req, res) => {
   })
   console.log(req.params.id);
   console.log(req.body);
-  res.json("Nota actualizada")
+  res.send("Nota actualizada")
 };
 
 notesCtrl.deleteNote = async (req, res) => {
-  await noteModel.findByIdAndDelete(req.params.id);
-  res.json("Nota eliminada")
+  await noteModel.findByIdAndDelete(req.params.id); //req.params.id consigue el id de la url
+  res.send("Nota eliminada")
 
 };
 
--- server/src/controllers/users_controller.js
+++ server/src/controllers/users_controller.js
@@ -1,9 +1,69 @@
-const usersCtrl = {
-  getAllUsers : (req,res) => res.json({message: 'GET USER Request'}),
-  createUser : (req, res) => res.json({message: 'POST usr Request'}),
-  updateUser : (req, res) => res.json({message: 'UpdAtE userr'}),
-  deleteUser : (req, res) => res.json({message: 'dleTE one uSER Request'}),
-  getOneUser : (req, res) => res.json({message: 'get just one Request'}),
+const usersCtrl = {};
+
+const userModel = require('../models/user');
+
+usersCtrl.getAllUsers = async (req,res) => {
+  const usuarios = await userModel.find();
+  res.json(usuarios)
 };
+
+usersCtrl.createUser = async (req, res) => {
+  const {username} = req.body;
+  const newUser = new userModel({
+    username: username
+  });
+  await newUser.save(
+    function(error){
+      if(error){
+        if (error.code == '11000'){
+          return res.status(500).json({success: false, message: 'Usuario repetido.'})
+        }else{ // cualquier otro error
+          return res.send(error)
+        }
+      }
+      res.json({message: "Usuario creado."})
+    });
+};
+
+usersCtrl.updateUser = async (req, res) => {
+  try {
+    const {username} = req.body;
+    const updateUser = await userModel.findByIdAndUpdate(req.params.id, {
+      username: username
+    })
+    console.log(updateUser);
+  } catch (error) {
+    console.log(error);
+    res.json(error.errmsg)
+  }
+
+  // await userModel.findByIdAndUpdate(req.params.id,
+  //   function(error){
+  //     const {username} = req.body;
+  //     username:username
+  //     if(error){
+  //       if (error.code == '11000'){
+  //         return res.status(500).json({success: false, message: 'Usuario repetido.'})
+  //       }else{ // cualquier otro error
+  //         return res.send(error)
+  //       }
+  //     }else{
+  //       username: username
+  //     }
+  //   }
+  // )
+  res.json({message: "Usuario actualizado."});
+},
+
+usersCtrl.deleteUser = async (req, res) => {
+  await userModel.findByIdAndDelete(req.params.id)
+  res.send("Usuario eliminado")
+};
+
+usersCtrl.getOneUser = async (req, res) => {
+  const user = await userModel.findById(req.params.id)
+  console.log(user);
+  res.json(user)
+},
 
 module.exports = usersCtrl;
