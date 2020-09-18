2ffd3c484ec25aa067f34118db1a7ae98106abc4
YuiZh0u <yui.zhou97@gmail.com>
2020-09-17 22:23:20 -0300

    mongodb integration

--- server/src/database.js
+++ server/src/database.js
@@ -1,1 +1,15 @@
 // Encargado de contener la conexion a la base de datos
+const mongoose = require('mongoose');
+
+const URI = 'mongodb://localhost/firstmern'; //crea una base de datos firstmern
+
+mongoose.connect(URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+})
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log('Conectado a MongoDB');
+})
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 // Archivo que va a arrancar el servidor
 
 const app = require('./app'); // importa app del archivo app.js
+require('./database');
 
 const port = 4000;
 app.listen(port);
