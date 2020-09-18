fd2003e07cc2779e322a877cb5e1b02edddb2574
YuiZh0u <yui.zhou97@gmail.com>
2020-09-17 23:05:26 -0300

    separate port in .env add middlewere add routes add script npm run dev

--- server/src/app.js
+++ server/src/app.js
@@ -2,5 +2,18 @@
 
 const express = require('express'); // Importar express
 const app = express(); // Llama a express
+const cors = require('cors');
+
+//Settings
+app.set('port', process.env.PORT || 4000)
+
+//Middlewere (funciones que se ejecuten antes de llegar a la ruta)
+app.use(cors());
+app.use(express.json());
+
+//Routes (url q el usuario puede acceder)
+
+app.get('/api/users', (req, res) => res.send('Ruta del usuario')) //es puramente nombre la ruta /api/ , se pone asi ya q se el servidor enviara json como si fuera una api
+app.get('/api/notes', (req, res) => res.send('Ruta para las notas'))
 
 module.exports = app; // Importa el modulo de express para ser llamado en index.js
--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,8 @@
 // Archivo que va a arrancar el servidor
 require('dotenv').config(); //importa variables de entorno
 
-
 const app = require('./app'); // importa app del archivo app.js
 require('./database');
 
-const port = 4000;
-app.listen(port);
+app.listen(app.get('port'));
+console.log(`Server on port ${app.get('port')}`);
