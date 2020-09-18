8f7c553f74c27ebfdbebaf7bc939320e2be22831
YuiZh0u <yui.zhou97@gmail.com>
2020-09-18 15:40:50 -0300

    add components structure and router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom' // BrowserRouter contenedor de la ruta, Route especifica la ruta
 import './App.css';
+
+import Nav from './components/Nav';
+import CreateNote from './components/CreateNote';
+import CreateUser from './components/CreateUser';
+import NotesList from './components/NotesList';
 
 function App() {
   return (
-    <div> Hola react</div>
+    <Router>
+      <Nav/>
+
+      <Route path="/" exact component={NotesList}/>
+      <Route path="/edit/:id" component={CreateNote}/>
+      <Route path="/create" component={CreateNote}/>
+      <Route path="/user" component={CreateUser}/>
+    </Router>
   );
 }
 
---
+++ client/src/components/Nav.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <div>
+        Nav bar component
+      </div>
+    )
+  }
+}
