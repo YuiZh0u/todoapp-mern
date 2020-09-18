8bd9773c1527e66a407c958d1c2b672750271126
YuiZh0u <yui.zhou97@gmail.com>
2020-09-17 23:32:39 -0300

    add basic HTTP request methods

--- server/src/routes/notes.js
+++ server/src/routes/notes.js
@@ -2,12 +2,12 @@ const { Router } = require('express'); //llamamos el objeto Router de express
 const router = Router(); //hacemos uso de la funcion Router => Enrutadores
 
 router.route('/') // hace referencia a la ruta definida en app.js (/api/notes)
-  .get((req,res) => res.send('hola'))
-//   .post()
+  .get((req,res) => res.json({message: 'GET Request'}))
+  .post((req, res) => res.json({message: 'POST Request'}))
 
-// router.route('/:id')
-//   .get()
-//   .put()
-//   .delete()
+router.route('/:id')
+  .get((req, res) => res.json({message: 'Datos de una nota especifica'}))
+  .put((req, res) => res.json({message: 'Nota aCtualizada'}))
+  .delete((req, res) => res.json({message: 'Nota eliminida'}))
 
 module.exports = router;
